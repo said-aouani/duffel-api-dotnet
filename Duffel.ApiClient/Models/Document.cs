fd840d05ce263e0c4c6849cae091afe9d4dbdf11
said-aouani <116915444+said-aouani@users.noreply.github.com>
2022-10-28 20:55:40 +0100

    Add files via upload
    The documents issued for this order

--- Duffel.ApiClient/Models/Document.cs
+++ Duffel.ApiClient/Models/Document.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace Duffel.ApiClient.Models
@@ -9,11 +10,17 @@ namespace Duffel.ApiClient.Models
         /// </summary>
         [JsonProperty("type")]
         public DocumentType DocumentType { get; set; }
-        
+
         /// <summary>
         /// The identifier for the document, in the case of electronic tickets this string represents the payment or the entitlement to fly.
         /// </summary>
         [JsonProperty("unique_identifier")]
         public string UId { get; set; }
+
+        /// <summary>
+        /// The list of passenger ids the document applies to
+        /// </summary>
+        [JsonProperty("passenger_ids")]
+        public List<string> PassengerIds { get; set; } = new List<string>();
     }
 }
